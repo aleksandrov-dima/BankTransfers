be4857d7dfc7c1d3b8347da95067b7edc6904e42
Aleksandrov Dima <i-dimka@hotmail.com>
2020-09-22 12:20:43 +0300

    First commit

---
+++ BankTransfers/Data/ApplicationDbContext.cs
@@ -0,0 +1,45 @@
+﻿using BankTransfers.Models.Security;
+using Microsoft.EntityFrameworkCore;
+
+namespace BankTransfers.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            //при создании БД добавляем роли Администратора и обычного пользователя
+            string adminRoleName = "admin";
+            string userRoleName = "user";
+
+            Role adminRole = new Role { Id = 1, Name = adminRoleName };
+            Role userRole = new Role { Id = 2, Name = userRoleName };
+            
+            modelBuilder.Entity<Role>().HasData(new Role[] { adminRole, userRole });
+
+
+            //добавляем для тестов пользователя с админской ролью
+            string adminEmail = "admin@mail.ru";
+            string adminPassword = "SuperAdmin3";
+
+            //получаем хэш пароля (+ соль)
+            var hashedPassword = PasswordHelper.GetSaltedHashedPassword(adminPassword, out string salt);
+
+            User adminUser = new User
+            {
+                Id = 1,
+                Email = adminEmail,
+                Password = hashedPassword,
+                Salt = salt,
+                RoleId = adminRole.Id
+            };
+
+            modelBuilder.Entity<User>().HasData(new User[] { adminUser });
+            
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
---
+++ BankTransfers/Models/Security/PasswordHelper.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace BankTransfers.Models.Security
+{
+    public class PasswordHelper
+    {
+        /// <summary>
+        /// Вычисление хэша пароля (с добавлением соли)
+        /// </summary>
+        /// <param name="password"></param>
+        /// <param name="s"></param>
+        /// <returns></returns>
+        public static string GetSaltedHashedPassword(string password, out string saltText)
+        {
+            //генерация соли
+            var rng = RandomNumberGenerator.Create();
+            var saltBytes = new byte[16];
+            rng.GetBytes(saltBytes);
+            saltText = Convert.ToBase64String(saltBytes);
+
+            //генерация соленого и хешированного пароля
+            var sha = SHA256.Create();
+            var saltedPassword = password + saltText;
+            var saltedhashedPassword =
+                Convert.ToBase64String(sha.ComputeHash(Encoding.Unicode.GetBytes(saltedPassword)));
+
+            return saltedhashedPassword;
+        }
+
+        /// <summary>
+        /// Проверка введенного пароля
+        /// Сравнение хеша паролей (с испольсованием соли пользователя)
+        /// </summary>
+        /// <param name="inputPassword"></param>
+        /// <param name="userPassword"></param>
+        /// <param name="userSalt"></param>
+        /// <returns></returns>
+        public static bool CheckPassword(string inputPassword, string userPassword, string userSalt)
+        {
+            //повторная генерация соленого и хешированного пароля
+            var sha = SHA256.Create();
+            var saltedPassword = inputPassword + userSalt;
+            var saltedHasgedPassword =
+                Convert.ToBase64String(sha.ComputeHash(Encoding.Unicode.GetBytes(saltedPassword)));
+
+            return (saltedHasgedPassword == userPassword);
+        }
+    }
+}
